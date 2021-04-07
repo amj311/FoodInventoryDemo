c7128a9f1745ab97f0739be1a60861fa10a7dda9
amj311 <amjudd315@gmail.com>
2021-04-06 19:42:03 -0600

    completed addItemActivity with scanning and receyclerview for added items

--- app/src/main/java/com/example/foodinventorydemo/AddItemActivity.java
+++ app/src/main/java/com/example/foodinventorydemo/AddItemActivity.java
@@ -1,21 +1,35 @@
 package com.example.foodinventorydemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentContainerView;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.foodinventorydemo.model.ProductUnitData;
 import com.example.foodinventorydemo.ui.scanner.ScannerCaller;
 import com.example.foodinventorydemo.ui.scanner.ScannerFragment;
+import com.example.foodinventorydemo.utils.ResourceResponseHandler;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,8 +38,20 @@ public class AddItemActivity extends AppCompatActivity {
     FrameLayout addFrag;
     MaterialButton scanBtn;
     ImageView closeBtn;
-    List<View> items;
+    EditText nameField;
+    EditText categoryField;
+    EditText expireField;
+    EditText qtyField;
+    ProgressBar searchingSpinner;
+    RecyclerView itemsRV;
+    ItemListAdapter adapter;
+    List<ProductUnitData> items = new ArrayList<>();
+    MaterialDatePicker datePicker;
     int numItemsShown = 0;
+    ScannerFragment scanner;
+    boolean searching = false;
+
+    ResourceResponseHandler<ProductUnitData> dataResHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,50 +59,213 @@ public class AddItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_item);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        dataResHandler = new ProductResultHandler();
 
         scanBtn = findViewById(R.id.scanButton);
         closeBtn = findViewById(R.id.closeBtn);
         fragWrap = findViewById(R.id.addScanFragWrapper);
         addFrag = findViewById(R.id.addScanFrag);
-        items = Arrays.asList(findViewById(R.id.fakeItem1), findViewById(R.id.fakeItem2),
-                findViewById(R.id.fakeItem3), findViewById(R.id.fakeItem4), findViewById(R.id.fakeItem5),
-                findViewById(R.id.fakeItem6), findViewById(R.id.fakeItem7));
+        searchingSpinner = findViewById(R.id.searchingSpinner);
+        nameField = findViewById(R.id.nameField);
+        categoryField = findViewById(R.id.categoryField);
+        expireField = findViewById(R.id.expField);
+        qtyField = findViewById(R.id.qtyField);
+
+        datePicker = MaterialDatePicker.Builder.datePicker().build();
+        datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                expireField.setText(datePicker.getHeaderText());
+            }
+        });
 
         scanBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Fragment f = ScannerFragment.newInstance(new AddScanCaller());
-                getSupportFragmentManager().beginTransaction().replace(R.id.addScanFrag, f).commit();
+                scanner = ScannerFragment.newInstance(new AddScanCaller());
+                getSupportFragmentManager().beginTransaction().replace(R.id.addScanFrag, scanner).commit();
                 fragWrap.setVisibility(View.VISIBLE);
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
             }
         });
         closeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getSupportFragmentManager().beginTransaction().remove(getSupportFragmentManager()
-                        .findFragmentById(R.id.addScanFrag)).commit();
+                getSupportFragmentManager().beginTransaction().remove(scanner).commit();
                 fragWrap.setVisibility(View.GONE);
+                scanner = null;
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
             }
         });
         findViewById(R.id.addItemBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                revealItem();
+                addItemFromForm();
             }
         });
+        expireField.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b) {
+                    datePicker.show(getSupportFragmentManager(),"ExpirationDatePicker");
+                    expireField.clearFocus();
+                }
+            }
+
+        });
+
+        adapter = new ItemListAdapter(items);
+        itemsRV = findViewById(R.id.itemsList);
+        itemsRV.setLayoutManager(new LinearLayoutManager(this));
+        itemsRV.setAdapter(adapter);
     }
 
-    private void revealItem() {
-        if (numItemsShown < items.size()) {
-            items.get(numItemsShown).setVisibility(View.VISIBLE);
-            numItemsShown++;
-        };
-    }
 
     class AddScanCaller extends ScannerCaller {
         @Override
         protected void onNewScanData(String data) {
-            revealItem();
+            if (searching) return;
+            induceSearchingState();
+            LookupCodeService.fetchProductData(data, dataResHandler);
         }
+    }
+
+    class ProductResultHandler extends ResourceResponseHandler<ProductUnitData> {
+        @Override
+        public void handleError(Exception e) {
+            Toast.makeText(AddItemActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            endSearchingState();
+        }
+
+        public void handleError(LookupCodeService.NotFoundException e) {
+            Toast.makeText(AddItemActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            endSearchingState();
+        }
+
+        @Override
+        public void handleRes(ProductUnitData res) {
+            endSearchingState();
+            addItem(res);
+        }
+    }
+
+    private void endSearchingState() {
+        searching = false;
+        scanner.allowScan();
+        searchingSpinner.setVisibility(View.GONE);
+    }
+
+
+
+
+    private void addItemFromForm() {
+        String name = nameField.getText().toString();
+        String expiration = expireField.getText().toString();
+        String category = categoryField.getText().toString();
+        int qty = 1;
+        try{
+            qty = Integer.parseInt(qtyField.getText().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        addItem(new ProductUnitData(null, name,null, null, expiration, category,null,null, qty));
+    }
+
+    private void addItem(ProductUnitData item) {
+        items.add(item);
+        adapter.notifyItemChanged(items.size()-1,item);
+        itemsRV.smoothScrollToPosition(items.size()-1);
+    }
+
+
+
+
+    private void induceSearchingState() {
+        searching = true;
+        scanner.disallowScan();
+        searchingSpinner.setVisibility(View.VISIBLE);
+    }
+
+
+
+
+
+    class ItemListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+        List<ProductUnitData> list;
+
+        public ItemListAdapter(List<ProductUnitData> items) {
+            list = items;
+        }
+
+        public class ItemViewHolder extends RecyclerView.ViewHolder {
+            private final TextView nameText;
+            private final TextView expireText;
+            private final TextView qtyText;
+            private final ImageView imageView;
+
+            public ItemViewHolder(@NonNull View itemView) {
+                super(itemView);
+                nameText = itemView.findViewById(R.id.productName);
+                expireText = itemView.findViewById(R.id.productExpire);
+                qtyText = itemView.findViewById(R.id.productQty);
+                imageView = itemView.findViewById(R.id.productImage);
+            }
+
+            public void setItem(ProductUnitData item) {
+                nameText.setText(item.getName());
+                expireText.setText(item.getExpiration()!=null ? item.getExpiration() : "Expires: 5/12/2024");
+                qtyText.setText(String.format("Qty: %d", item.getQty()!=0 ? item.getQty() : 1));
+                attemptSetImage(imageView, item,null);
+            }
+
+            private void attemptSetImage(final ImageView imageView, final ProductUnitData item, final String lastUrl) {
+                final String url = item.getNextUrl(lastUrl);
+                if (url == null) {
+                    imageView.setImageResource(R.drawable.common_google_signin_btn_icon_disabled);
+                    return;
+                }
+                Picasso.get().load(url).into(imageView, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Log.e("PICASSO_ERROR","Failed to load image: "+lastUrl);
+                        attemptSetImage(imageView, item, url);
+                    }
+                });
+            }
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view, parent, false);
+            return new ItemViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            ItemViewHolder itemHolder = (ItemViewHolder) holder;
+            itemHolder.setItem(list.get(position));
+        }
+
+        /**
+         * Returns the total number of items in the data set held by the adapter.
+         *
+         * @return The total number of items in this adapter.
+         */
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/foodinventorydemo/utils/ResourceResponseHandler.java
@@ -0,0 +1,6 @@
+package com.example.foodinventorydemo.utils;
+
+public class ResourceResponseHandler<T> {
+    public void handleError(Exception e) {};
+    public void handleRes(T res) {};
+}
