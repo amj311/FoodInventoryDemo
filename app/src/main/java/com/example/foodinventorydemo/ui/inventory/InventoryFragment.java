edc122a4e2c7b55b81df6210c5c4cc05cfbc784e
amj311 <amjudd315@gmail.com>
2021-04-12 18:24:18 -0600

    implemented exp alert in inventory list

--- app/src/main/java/com/example/foodinventorydemo/ui/inventory/InventoryFragment.java
+++ app/src/main/java/com/example/foodinventorydemo/ui/inventory/InventoryFragment.java
@@ -20,6 +20,11 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.foodinventorydemo.R;
 import com.squareup.picasso.Picasso;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -31,22 +36,22 @@ public class InventoryFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_inventory, container, false);
 
         ArrayList<FoodItemDisplayable> grainList = new ArrayList<>();
-        grainList.add(new FoodItemDisplayable("https://previews.123rf.com/images/jamakosy/jamakosy1711/jamakosy171100225/90332158-white-rice-in-burlap-sack-bag-isolated-on-white-background.jpg","Rice", "05/07/2021", "4"));
-        grainList.add(new FoodItemDisplayable("https://www.dutchmansstore.com/wp-content/uploads/2020/09/DSC_4812-Edit-scaled-e1601073911963.jpg","Noodles", "05/07/2021", "3"));
+        grainList.add(new FoodItemDisplayable("https://previews.123rf.com/images/jamakosy/jamakosy1711/jamakosy171100225/90332158-white-rice-in-burlap-sack-bag-isolated-on-white-background.jpg","Rice", "05/07/2025", "4"));
+        grainList.add(new FoodItemDisplayable("https://www.dutchmansstore.com/wp-content/uploads/2020/09/DSC_4812-Edit-scaled-e1601073911963.jpg","Noodles", "05/07/2025", "3"));
 
         ArrayList<FoodItemDisplayable> fruitList = new ArrayList<>();
-        fruitList.add(new FoodItemDisplayable("https://nuts.com/images/rackcdn/ed910ae2d60f0d25bcb8-80550f96b5feb12604f4f720bfefb46d.ssl.cf1.rackcdn.com/e8d95ad6e4090391-sXRwtvud-large.jpg","Dried Apples", "08/20/2021", "5"));
+        fruitList.add(new FoodItemDisplayable("https://nuts.com/images/rackcdn/ed910ae2d60f0d25bcb8-80550f96b5feb12604f4f720bfefb46d.ssl.cf1.rackcdn.com/e8d95ad6e4090391-sXRwtvud-large.jpg","Dried Apples", "04/30/2021", "5"));
         fruitList.add(new FoodItemDisplayable("https://www.bellaviva.com/assets/images/dried-fruit/pear.jpg","Dried Pears", "08/20/2021", "6"));
 
         ArrayList<FoodItemDisplayable> otherList = new ArrayList<>();
         otherList.add(new FoodItemDisplayable("https://www.peanutbutter.com/wp-content/uploads/2019/03/SKIPPY_Product_PB_Spread_Creamy_Peanut_Butter_28oz.png","Peanut Butter", "04/21/2021", "7"));
-        otherList.add(new FoodItemDisplayable("https://images.heb.com/is/image/HEBGrocery/000567987","Bottled Water", "08/20/2021", "2"));
+        otherList.add(new FoodItemDisplayable("https://images.heb.com/is/image/HEBGrocery/000567987","Bottled Water", "08/20/2023", "2"));
         otherList.add(new FoodItemDisplayable("https://images.homedepot-static.com/productImages/741fbb97-dedc-4571-acf1-9856fe6ec0ea/svn/camco-toilet-paper-40274-64_1000.jpg","Toilet Paper", "08/14/2054", "145"));
 
         HashMap<String, ArrayList<FoodItemDisplayable>> listData = new HashMap<>();
 
-        listData.put("Grains", grainList);
         listData.put("Fruit", fruitList);
+        listData.put("Grains", grainList);
         listData.put("Other", otherList);
 
         List<String> keyList = new ArrayList<>(listData.keySet());
@@ -101,6 +106,7 @@ public class InventoryFragment extends Fragment {
             public TextView expirationDate;
             public TextView itemQuantity;
             public ImageView trashCan;
+            public ImageView expAlertIcon;
 //            public NumberPicker numberPicker;
             public FoodItemDisplayable displayable;
 
@@ -110,6 +116,7 @@ public class InventoryFragment extends Fragment {
                 expirationDate = view.findViewById(R.id.expiration_date);
                 itemQuantity = view.findViewById(R.id.item_quantity);
                 trashCan = view.findViewById(R.id.list_item_trash);
+                expAlertIcon = view.findViewById(R.id.exp_alert_icon);
 //                numberPicker = view.findViewById(R.id.list_number_picker);
                 displayable = null;
                 view.setOnClickListener(new View.OnClickListener() {
@@ -196,7 +203,6 @@ public class InventoryFragment extends Fragment {
             String url = displayable.url;
             Picasso.get().load(url).into(viewHolder.icon);
             viewHolder.foodName.setText(displayable.displayFoodName());
-            viewHolder.expirationDate.setText(displayable.displayExpirationDate());
             viewHolder.itemQuantity.setText(displayable.displayQuantity());
             viewHolder.trashCan.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -209,6 +215,19 @@ public class InventoryFragment extends Fragment {
 //            viewHolder.numberPicker.setMaxValue(100);
 //            viewHolder.numberPicker.setMinValue(0);
             viewHolder.displayable = displayable;
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/uuuu");
+            LocalDate expDate = LocalDate.parse(displayable.expirationDate, formatter);
+            LocalDate today = LocalDate.now();
+            long numDays = ChronoUnit.DAYS.between(today, expDate);
+            System.out.println("Number of days between two dates = "+numDays);
+            viewHolder.expirationDate.setText(displayable.displayExpirationDate());
+
+            if (numDays <= 31) {
+                viewHolder.expAlertIcon.setVisibility(View.VISIBLE);
+                viewHolder.expirationDate.setTextColor(getResources().getColor(R.color.out_red));
+            }
+
             return rowView;
         }
 
